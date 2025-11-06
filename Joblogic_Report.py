78ae6bef7bdbea8fea16f7a38843e7bdfc6fa64a
Ronnie704 <ronnie@flowrightcommercial.com>
2025-11-06 09:43:26 +0000

    Update Joblogic_Report.py

--- Joblogic_Report.py
+++ Joblogic_Report.py
@@ -101,7 +101,7 @@ def transform_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
         if {"Time on Site", "Time off Site"}.issubset(df.columns):
             duration = df["Time off Site"] - df["Time on Site"]
-            df["_job_hours"] = (duration.dt.toal_seconds() / 3600).fillna(0)
+            df["_job_hours"] = (duration.dt.total_seconds() / 3600).fillna(0)
         else:
             df["_job_hours"] = 0.0
 
