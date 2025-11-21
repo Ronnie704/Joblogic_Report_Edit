f98611007e01b32666aaa5ebd17b362c8867a5b4
Ronnie704 <ronnie@flowrightcommercial.com>
2025-11-21 22:43:28 +0000

    Still Updatinf.py

--- Joblogic_Report.py
+++ Joblogic_Report.py
@@ -188,7 +188,7 @@ def transform_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         jt = df["Job Travel"]
 
         real_date = np.where(
-            jt.dt.hour < 6,
+            jt.dt.hour < 4,
             (jt - pd.Timedelta(days=1)).dt.date,   # shift back one day
             jt.dt.date
         )
